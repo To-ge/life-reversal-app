004301c872dd6293f73f177d0542b1a183a7efcd
to-ge <tougei324@gmail.com>
2023-08-01 08:37:06 +0900

    to ssr from ssg

--- frontend/src/pages/search/index.tsx
+++ frontend/src/pages/search/index.tsx
@@ -1,8 +1,8 @@
 import ScrollArea from "components/sns/ScrollArea";
 import TopBar from "components/topbar/TopBar";
-import { GetStaticProps, InferGetStaticPropsType } from "next";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
-export const getStaticProps: GetStaticProps = async (ctx) => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const articleResponse = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/api/v1/articles`
   );
@@ -16,17 +16,17 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
 
   return {
     props: {
-      articles,
-      users,
+      data: {
+        articles,
+        users,
+      },
     },
-    revalidate: 60 * 60 * 24,
   };
 };
 
-const Page = ({
-  articles,
-  users,
-}: InferGetStaticPropsType<UsersAndArticles>) => {
+const Page = ({ data }: InferGetServerSidePropsType<UsersAndArticles>) => {
+  const { articles, users } = data;
+  console.log(articles, users);
   return (
     <div className="h-screen w-screen fixed">
       <TopBar />
