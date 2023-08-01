beca408b6a6a85622a143ca7cb206bf34bfd439f
to-ge <tougei324@gmail.com>
2023-08-01 12:21:12 +0900

    add in readme and fix style

--- frontend/src/components/post/tree-view/TreeView.tsx
+++ frontend/src/components/post/tree-view/TreeView.tsx
@@ -18,13 +18,13 @@ const TreeView = (props: TreeViewProps) => {
     <div className="w-2/3 bg-gradient-to-r from-gray-300 to-slate-600 flex justify-center">
       <ul className="w-3/4 flex flex-col items-center p-5 space-y-10 overflow-y-auto mb-20">
         {cardInfo.map((card, index) => (
-          <li key={index} className="w-3/4 shadow-xl">
+          <li key={index} className="w-full shadow-xl">
             <p className="w-20 text-lg ml-8 bg-white flex justify-center rounded-t-lg">
               {`No.${index + 1}`}
             </p>
             <input
               type="text"
-              className="p-7 text-2xl w-full focus:outline-none rounded-lg"
+              className="py-5 px-7 text-md w-full focus:outline-none rounded-lg"
               placeholder="when, where, how"
               onChange={(e) => handleChange(index, e.target.value)}
               value={card.text}
